8b04d0a1e111a70af3e9b0429e8056aa9e8745f8
Joe-noh <goflb.jh@gmail.com>
2016-03-28 23:50:17 +0900

    import files from Joe-noh/phaser-tutorial

---
+++ src/main.js
@@ -0,0 +1,15 @@
+let game = new Phaser.Game(400, 490);
+
+let mainState = {
+  preload() {
+  },
+
+  create() {
+  },
+
+  update() {
+  },
+};
+
+game.state.add('main', mainState);
+game.state.start('main');
