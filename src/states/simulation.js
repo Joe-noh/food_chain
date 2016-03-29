90e9a54c183c3f79cc19972f43fcd5d3f0f4dbe0
Joe-noh <honzawa.j@pepabo.com>
2016-03-29 09:33:45 +0900

    state transition and passing parameters

---
+++ src/states/simulation.js
@@ -0,0 +1,20 @@
+let simulationState = {
+  init(params) {
+      console.log(params);
+  },
+
+  preload() {
+  },
+
+  create() {
+    console.log("simulationState created");
+  },
+
+  update() {
+  },
+
+  startGame() {
+  }
+};
+
+export default simulationState;
